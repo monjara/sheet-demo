a56a400487766bff9e21168929070c1e191c479c
zutoy <yaginumtt@gmail.com>
2024-10-23 22:08:53 +0900

    add types

---
+++ src/features/canvas_datagrid/canvas_data_grid.d.ts
@@ -0,0 +1,8 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    'canvas-datagrid': {
+      ref: React.RefObject<HTMLCanvasElement>
+      data: string
+    }
+  }
+}
--- src/features/canvas_datagrid/index.tsx
+++ src/features/canvas_datagrid/index.tsx
@@ -3,26 +3,12 @@ import { useRef, useState } from 'react'
 
 export default function CanvasDataGrid() {
   const ref = useRef(null)
-  const [data, setData] = useState([
-    { A: 'column1', B: 'column1' },
-    { A: 'column2' },
-  ])
-  const btnClickFunc = () => {
-    // ※ ref にてテーブル内のデータにアクセス可能。書き換えも可能。
-    if (!ref.current) return
-    console.log(ref.current)
-    console.log(typeof ref.current)
-    //ref.current.data.splice(1)
-    //setGridData(ref.current.data)
-  }
+  const [data] = useState([{ A: 'column1', B: 'column1' }, { A: 'column2' }])
+
   return (
     <div>
       <h1>CanvasDataGrid</h1>
-      <canvas-datagrid
-        ref={ref}
-        //style={{ height: '', width: '100%' }}
-        data={JSON.stringify(data)}
-      />
+      <canvas-datagrid ref={ref} data={JSON.stringify(data)} />
     </div>
   )
 }
