bc97ff81a819b29d0293d0c2b8e3b37443cbe2e6
zutoy <yaginumtt@gmail.com>
2024-10-27 16:00:31 +0900

    mode

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? 'https://zutoy.github.io/sheet-demo/' : '/',
+  base: mode === 'production' ? '/sheet-demo' : '/',
   plugins: [react()],
 }))
