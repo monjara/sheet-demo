c5a28249aa078ebb4985ac70c6cd78e3048bcf50
zutoy <yaginumtt@gmail.com>
2024-10-27 15:36:15 +0900

    editable

--- src/features/grid/index.tsx
+++ src/features/grid/index.tsx
@@ -1,62 +1,132 @@
-import Grid, { Cell, useEditable, type RendererProps } from '@rowsncolumns/grid'
-import { useRef, useState } from 'react'
+import Grid, {
+  Cell,
+  useEditable,
+  useSelection,
+  type CellInterface,
+  type RendererProps,
+} from '@rowsncolumns/grid'
+import { useCallback, useMemo, useRef, useState } from 'react'
 import { makeData, type Person } from '../../makeData'
 
-function renderCell({ data, ...props }: RendererProps & { data: Person[] }) {
-  const makeValue = () => {
-    const d = {
-      '0': props.columnIndex.toString(),
-      '1': data[props.columnIndex].firstName,
-      '2': data[props.columnIndex].lastName,
-      '3': data[props.columnIndex].age.toString(),
-      '4': data[props.columnIndex].visits.toString(),
-      '5': data[props.columnIndex].progress.toString(),
-      '6': data[props.columnIndex].status.toString(),
-      '7': data[props.columnIndex].createdAt.toISOString(),
-      '8': data[props.columnIndex].createdAt.toISOString(),
-      '9': data[props.columnIndex].createdAt.toISOString(),
-      '10': data[props.columnIndex].createdAt.toISOString(),
-      '11': data[props.columnIndex].createdAt.toISOString(),
-      '12': data[props.columnIndex].createdAt.toISOString(),
-      '13': data[props.columnIndex].createdAt.toISOString(),
-      '14': data[props.columnIndex].createdAt.toISOString(),
-      '15': data[props.columnIndex].createdAt.toISOString(),
-      '16': data[props.columnIndex].createdAt.toISOString(),
-      '17': data[props.columnIndex].createdAt.toISOString(),
-      '18': data[props.columnIndex].createdAt.toISOString(),
-      '19': data[props.columnIndex].createdAt.toISOString(),
-      '20': data[props.columnIndex].createdAt.toISOString(),
-      ...Array.from({ length: 200 }, (_, i) => [
-        i + 21,
-        data[props.columnIndex].createdAt.toISOString(),
-      ]).reduce((acc, [key, value]) => {
-        // @ts-ignore
-        acc[key] = value
-        return acc
-      }, {}),
-    }[props.rowIndex]
+function renderCell({
+  data,
+  makeValue,
+  ...props
+}: RendererProps & {
+  data: Person[]
+  makeValue: (rowIndex: number, columnIndex: number) => string
+}) {
+  return (
+    <Cell {...props} value={makeValue(props.columnIndex, props.rowIndex)} />
+  )
+}
+const order = [
+  'firstName',
+  'lastName',
+  'age',
+  'visits',
+  'progress',
+  'status',
+  'createdAt',
+  'param_1',
+  'param_2',
+  'param_3',
+  'param_4',
+  'param_5',
+  'param_6',
+  'param_7',
+  'param_8',
+  'param_9',
+  'param_10',
+  'param_11',
+  'param_12',
+  'param_13',
+  'param_14',
+  'param_15',
+  'param_16',
+  'param_17',
+  'param_18',
+  'param_19',
+  'param_20',
+  'param_21',
+  'param_22',
+  'param_23',
+  'param_24',
+  'param_25',
+]
 
-    return d ?? ''
-  }
-  return <Cell {...props} value={makeValue()} />
+function getValue(modified: string[][], cell: CellInterface) {
+  return modified[cell.columnIndex][cell.rowIndex]
 }
 
 export default function RowColumnsGrid() {
-  const [data, _] = useState(() => makeData(10000))
   const gridRef = useRef(null)
+  const [data, _] = useState(() => makeData(10000))
+  const rowCount = 230
+  const columnCount = 10000
+
+  const modified = useMemo(() => {
+    const retval: string[][] = []
+    let i = 0
+    for (const d of data) {
+      retval[i] = []
+      let j = 0
+      for (const o of order) {
+        retval[i][j] = d[o as keyof Person].toString()
+        j++
+      }
+      i++
+    }
+    return retval
+  }, [data])
+
+  const makeValue = useCallback(
+    (columnIndex: number, rowIndex: number) => {
+      return modified?.[columnIndex]?.[rowIndex] ?? ''
+    },
+    [modified]
+  )
+
+  const { selections } = useSelection({
+    gridRef,
+    rowCount,
+    columnCount,
+    getValue: (cell: CellInterface) => getValue(modified, cell),
+  })
+
+  // @ts-ignore
+  const { editorComponent, ...editableProps } = useEditable({
+    gridRef,
+    selections,
+    columnCount,
+    rowCount,
+    getValue: (cell: CellInterface) => getValue(modified, cell),
+    onSubmit: (value: string, cell: CellInterface) => {
+      modified[cell.columnIndex][cell.rowIndex] = value
+    },
+  })
 
   return (
-    <Grid
-      ref={gridRef}
-      rowCount={230}
-      columnCount={10000}
-      width={1000}
-      height={600}
-      rowHeight={() => 20}
-      columnWidth={() => 100}
-      itemRenderer={(props) => renderCell({ data, ...props })}
-      enableCellOverlay
-      enableSelectionDrag
-    />
+    <div
+      style={{
+        position: 'relative',
+      }}
+    >
+      <Grid
+        ref={gridRef}
+        selections={selections}
+        rowCount={230}
+        columnCount={10000}
+        width={1000}
+        height={600}
+        rowHeight={() => 20}
+        columnWidth={() => 100}
+        itemRenderer={(props) => renderCell({ data, makeValue, ...props })}
+        enableCellOverlay
+        enableSelectionDrag
+        {...editableProps}
+      />
+      {editorComponent}
+    </div>
   )
 }
--- src/makeData.ts
+++ src/makeData.ts
@@ -8,6 +8,31 @@ export type Person = {
   visits: number
   progress: number
   status: 'relationship' | 'complicated' | 'single'
+  param_1: number
+  param_2: number
+  param_3: number
+  param_4: number
+  param_5: number
+  param_6: number
+  param_7: number
+  param_8: number
+  param_9: number
+  param_10: number
+  param_11: number
+  param_12: number
+  param_13: number
+  param_14: number
+  param_15: number
+  param_16: number
+  param_17: number
+  param_18: number
+  param_19: number
+  param_20: number
+  param_21: number
+  param_22: number
+  param_23: number
+  param_24: number
+  param_25: number
   createdAt: Date
 }
 
@@ -30,6 +55,31 @@ const newPerson = (index: number): Person => {
     progress: faker.number.int(100),
     createdAt: d,
     status: 'relationship',
+    param_1: 0,
+    param_2: 0,
+    param_3: 0,
+    param_4: 0,
+    param_5: 0,
+    param_6: 0,
+    param_7: 0,
+    param_8: 0,
+    param_9: 0,
+    param_10: 0,
+    param_11: 0,
+    param_12: 0,
+    param_13: 0,
+    param_14: 0,
+    param_15: 0,
+    param_16: 0,
+    param_17: 0,
+    param_18: 0,
+    param_19: 0,
+    param_20: 0,
+    param_21: 0,
+    param_22: 0,
+    param_23: 0,
+    param_24: 0,
+    param_25: 0,
   }
 }
 
